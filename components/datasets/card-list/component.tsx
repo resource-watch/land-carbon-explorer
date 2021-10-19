e650dea7967d5b0cbd4c168d204ff1f694fed2ea
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-10-19 19:23:49 +0200

    WIP

--- components/datasets/card-list/component.tsx
+++ components/datasets/card-list/component.tsx
@@ -10,8 +10,10 @@ import DatasetListItem from '../card-item';
 
 export interface DatasetCardListProps {
   list: Dataset[];
+  expandedChart?: boolean;
 }
 
+<<<<<<< HEAD
 export const DatasetCardList: FC<DatasetCardListProps> = ({ list }: DatasetCardListProps) => (
   <>
     {list.map((dataset) => (
@@ -20,10 +22,33 @@ export const DatasetCardList: FC<DatasetCardListProps> = ({ list }: DatasetCardL
           dataset={dataset}
           layer={dataset.layer ? dataset.layer.find((l) => l.default) : null}
           metadata={dataset.metadata}
+=======
+export const DatasetCardList: FC<DatasetCardList> = ({
+  list,
+  expandedChart,
+}) => (
+    <>
+    {list.map((dataset) => (
+      <div
+        className="dataset-card-list-item-container mt-3 first:mt-0"
+        key={dataset.id}
+      >
+        <DatasetListItem
+          dataset={dataset}
+          widget={dataset.widget ? dataset.widget.find((w) => w.default) : null}
+          layer={dataset.layer ? dataset.layer.find((l) => l.default) : null}
+          metadata={dataset.metadata && Array.isArray(dataset.metadata)
+            ? dataset.metadata[0] : dataset.metadata}
+          expandedChart={expandedChart}
+>>>>>>> 7d6ca14 (WIP)
         />
       </div>
     ))}
   </>
+<<<<<<< HEAD
 );
+=======
+)
+>>>>>>> 7d6ca14 (WIP)
 
 export default DatasetCardList;
