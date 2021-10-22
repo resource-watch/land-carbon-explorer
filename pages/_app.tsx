62ecdfe0f480076a5b267e25175c1c473ea50c27
David Inga <david.inga@vizzuality.com>
2021-10-22 12:57:42 +0200

    removed next api pages; removed next-auth dependencies and components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { Provider as ReduxProvider } from 'react-redux';
 import type { AppProps } from 'next/app';
 
 import { OverlayProvider } from '@react-aria/overlays';
-import { Provider as AuthenticationProvider } from 'next-auth/client';
 import { Hydrate } from 'react-query/hydration';
 
 import store from 'store';
@@ -17,17 +16,9 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   <ReduxProvider store={store}>
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <AuthenticationProvider
-          session={pageProps.session}
-          options={{
-            clientMaxAge: 5 * 60, // Re-fetch session if cache is older than 60 seconds
-            keepAlive: 10 * 60, // Send keepAlive message every 10 minutes
-          }}
-        >
-          <OverlayProvider>
-            <Component {...pageProps} />
-          </OverlayProvider>
-        </AuthenticationProvider>
+        <OverlayProvider>
+          <Component {...pageProps} />
+        </OverlayProvider>
       </Hydrate>
     </QueryClientProvider>
   </ReduxProvider>
